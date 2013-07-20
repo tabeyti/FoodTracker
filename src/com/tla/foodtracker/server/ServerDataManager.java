28c8c670aadc14e23ce24eb9f6516b3fbd72c8e7
Travis A <tabeyti@gmail.com>
2013-07-19 21:24:40 -0700

    Removed unecessary comments within file and removed ugly shadow I forgot I applied to the bottom panel of DayEntryPanel

--- src/com/tla/foodtracker/server/ServerDataManager.java
+++ src/com/tla/foodtracker/server/ServerDataManager.java
@@ -17,10 +17,10 @@ import com.tla.foodtracker.shared.Globals;
 
 public class ServerDataManager extends RemoteServiceServlet implements RPCInterface
 {
-	private static String logPath = "logs/";
-	private static String datPath = "dat/";
-//	private static String logPath = "C:/ServerFiles/FoodTracker/logs/";
-//	private static String datPath = "C:/ServerFiles/FoodTracker/dat/";
+//	private static String logPath = "logs/";
+//	private static String datPath = "dat/";
+	private static String logPath = "C:/ServerFiles/FoodTracker/logs/";
+	private static String datPath = "C:/ServerFiles/FoodTracker/dat/";
 
 	
 	@Override
@@ -60,35 +60,16 @@ public class ServerDataManager extends RemoteServiceServlet implements RPCInterf
 	{
 		byte[] data = null;
 		Path path = null;
-		
-//		BufferedWriter writer = null;
-//		
-//		try
-//		{
-//			writer = new BufferedWriter(new FileWriter("debug.log"));
-//			writer.write("getFile()\n");
-//		} catch (IOException e1)
-//		{
-//			// TODO Auto-generated catch block
-//			e1.printStackTrace();
-//		}
-//		
-		
-		
+
 		// sets resource variables
 		try
-		{
-//			writer.write("entered try\n");
-			
+		{			
 			// determines the file path based on the type of file passed in
 			switch (ft)
 			{
 				case GOALS:
 				case FOODLIST:
-//					writer.write("entered FOODLIST case\n");
 					path = Paths.get(datPath + fileName + Globals.extention);
-//					writer.write("leaving FOODLIST case\n");
-//					return new Data("<FoodList><Food><Name>Almond Crunch Bar</Name><Unit>bar</Unit><Calories>240.0</Calories><Protein>0.0</Protein><Carbohydrates>10.0</Carbohydrates><Fats>22.0</Fats></Food></FoodList>".getBytes(), "foodList");
 					break;
 				case LOGENTRY:
 					path = Paths.get(logPath + fileName + Globals.extention);
@@ -96,19 +77,10 @@ public class ServerDataManager extends RemoteServiceServlet implements RPCInterf
 			}
 			
 			data = Files.readAllBytes(path);
-//			writer.write("leaving getFile()\n");
 			
 		}
 		catch (Exception e)
 		{
-//			try
-//			{
-//				writer.write("Exception: " + e.getMessage() + "\n" + e.getStackTrace());
-//			} catch (IOException e1)
-//			{
-//				// TODO Auto-generated catch block
-//				e1.printStackTrace();
-//			}
 			data = null;
 			return new Data(data, fileName);
 		}
